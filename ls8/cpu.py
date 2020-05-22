ff630ac33d80b944e6ccbfbe3d15483c805e2f31
Abdi Farah <abdirahman.z.farah@gmail.com>
2020-05-21 19:37:56 -0700

    MVP day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,8 @@
 
 import sys
 
+SP = 7
+
 
 class CPU:
     """Main CPU class."""
@@ -11,6 +13,11 @@ class CPU:
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
+        self.reg[SP] = 0xF4
+        self.flag = [0] * 8
+        self.flag_equal = 0
+        self.flag_less = 0
+        self.flag_greater = 0
 
     def ram_read(self, mar):
         return self.ram[mar]
@@ -55,6 +62,18 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         # elif op == "SUB": etc
+        elif op == "MULT":
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "CMP":
+            if self.reg[reg_a] = self.reg[reg_b]:
+                self.flag_equal = 1
+            elif self.reg[reg_a] < self.reg[reg_b]:
+                self.flag_less = 1
+            elif self.reg[reg_a] > self.reg[reg_b]:
+                self.flag_greater = 1
+            else:
+                return 0
+
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -81,14 +100,20 @@ class CPU:
     def run(self):
         """Run the CPU."""
 
-        HLT = 0b00000001
-        LDI = 0b10000010
-        PRN = 0b01000111
-        MULT = 0b10100010
+        HLT = 0b00000001  # 1
+        LDI = 0b10000010  # 126
+        PRN = 0b01000111  # 71
+        MULT = 0b10100010  #
+        PUSH = 0b01000101
+        POP = 0b01000110
+        CALL = 0b01010000
+        RET = 0b00010001
+        ADD = 0b10100000
 
         running = True
-
+        # instruction = None
         while running:
+            # print("instruction", instruction)
             instruction = self.ram_read(self.pc)
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
@@ -108,10 +133,48 @@ class CPU:
                 print(self.reg[operand_a])
                 self.pc += 2
             elif instruction == MULT:
-                print(self.reg[operand_a] * self.reg[operand_b])
+                self.alu("MULT", operand_a, operand_b)
                 self.pc += 3
 
+            elif instruction == ADD:
+                self.alu("ADD", operand_a, operand_b)
+                self.pc += 3
+
+            elif instruction == POP:
+                top_of_stack_addr = self.reg[SP]
+
+                value = self.ram_read(top_of_stack_addr)
+
+                self.reg[operand_a] = value
+
+                self.reg[SP] += 1
+
+                self.pc += 2
+
+            elif instruction == PUSH:
+                # Decrement the SP
+                self.reg[SP] -= 1
+                # Get value out of register
+                data = self.reg[operand_a]
+                # store value in memory at SP
+                top_of_stack_addr = self.reg[SP]
+                self.ram_write(top_of_stack_addr, data)
+                self.pc += 2
+            elif instruction == CALL:
+                get_addr = self.pc + 2
+
+                # Push it on the Stack
+                self.reg[SP] -= 1
+                top_of_stack_addr = self.reg[SP]
+                self.ram[top_of_stack_addr] = get_addr
+                self.pc = self.reg[operand_a]
+            elif instruction == RET:
+                top_of_stack_addr = self.reg[SP]
+                self.pc = self.ram[top_of_stack_addr]
+                self.reg[SP] += 1
+
             else:
-                print(f'unknown instruction {instruction, LDI }')
+                # print(self.pc, self.reg, self.ram, "pc, ram, reg")
+                print(f'unknown instruction {instruction, CALL }')
                 running: False
                 sys.exit()
